1532bacacea3c895234d2e077be48ad3f62c3d8f
darrenmoriarty <dmamprop@gmail.com>
2022-07-02 08:37:45 +0100

    add tailwind and product template

---
+++ pages/product/[slug].tsx
@@ -0,0 +1,8 @@
+import { InferGetStaticPropsType } from "next/types";
+import Product from "../../components/Product";
+
+export default function Slug({product}) {
+    return (
+        <Product />
+    )
+}
