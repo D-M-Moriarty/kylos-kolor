ca4b833f6ea521f2494cc9b90cfe271c119701c6
darrenmoriarty <dmamprop@gmail.com>
2022-07-26 23:39:32 +0100

    added tuikit grid for products

---
+++ pages/product/index.tsx
@@ -0,0 +1,8 @@
+import { NextPage } from 'next'
+import Productgrid8 from '@components/grid/ProductGrid.tiukit'
+
+const Grid: NextPage = () => {
+    return <Productgrid8 />
+}
+
+export default Grid
